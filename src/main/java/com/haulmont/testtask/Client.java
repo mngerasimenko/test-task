a707dca1d6e11c6316e8520e9e66d186a3784235
mngerasimenko <mngerasimenko@gmail.com>
2021-03-18 21:58:57 +0400

    added getters and setters in Client

--- src/main/java/com/haulmont/testtask/Client.java
+++ src/main/java/com/haulmont/testtask/Client.java
@@ -7,4 +7,52 @@ public class Client {
     private String telephone;
     private String email;
     private String passport;
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public String getForeName() {
+        return foreName;
+    }
+
+    public void setForeName(String foreName) {
+        this.foreName = foreName;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassport() {
+        return passport;
+    }
+
+    public void setPassport(String passport) {
+        this.passport = passport;
+    }
 }
